28566cf2b8081e1be96b24f07454245fde974b28
WrobelXXL <michaschne@outlook.de>
2026-08-04 11:18:18 +0200

    first commit

---
+++ src/client/java/net/clanimg/remote_control/client/Remote_controlClient.java
@@ -0,0 +1,10 @@
+package net.clanimg.remote_control.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Remote_controlClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/clanimg/remote_control/Remote_control.java
@@ -0,0 +1,10 @@
+package net.clanimg.remote_control;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Remote_control implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
